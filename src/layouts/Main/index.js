be0b51bb736d8aeb584b04917f9a8200424c3463
Alexander <alexander@MacBook-Pro-Alexander.local>
2021-08-05 16:27:06 +0300

    (4) Added i18n. Updated main layout.

--- src/layouts/Main/index.js
+++ src/layouts/Main/index.js
@@ -1,15 +1,14 @@
 import React, { Suspense } from "react";
 // Core material
-import { CssBaseline, makeStyles, AppBar, Toolbar, Typography } from "@material-ui/core";
-import Sidebar from "components/Sidebar";
+import { CssBaseline, makeStyles, Toolbar } from "@material-ui/core";
+// Components
+import { Sidebar, TopBar } from "components";
+// Router
 import { renderRoutes } from "react-router-config";
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
   },
   content: {
     flexGrow: 1,
@@ -23,13 +22,7 @@ export default function MainLayout({ route }) {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            Clipped drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <TopBar />
 
       <Sidebar />
 
