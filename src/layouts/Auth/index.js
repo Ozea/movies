2952247bf7b2c25e5a4929bb59d53443af91775f
Alexander <alexander@MacBook-Pro-Alexander.local>
2021-12-02 23:45:53 +0200

    Added login page with dummy authorization

--- src/layouts/Auth/index.js
+++ src/layouts/Auth/index.js
@@ -2,8 +2,12 @@ import React, { Suspense } from "react";
 // Styles
 import { makeStyles } from '@mui/styles';
 // Assets
-import AuthImage from "assets/auth.jpg";
+import AuthImage from "assets/auth-bg.jpg";
 import { renderRoutes } from "react-router-config";
+import { AppBar, Button, Typography } from "@mui/material";
+import { GridContainer } from "components";
+import { GridItem } from "components";
+import { HowToReg } from "@mui/icons-material";
 
 const useStyles = makeStyles(theme => ({
   wrapper: {
@@ -12,6 +16,18 @@ const useStyles = makeStyles(theme => ({
     height: '100%',
     backgroundSize: 'cover',
     backgroundPosition: 'center'
+  },
+  mask: {
+    position: 'absolute',
+    top: '0px',
+    left: '0px',
+    width: '100%',
+    height: '100%',
+    zIndex: '0',
+    background: 'linear-gradient(to top, #2F3B52A3, #2F3B5294, #8080807d)',
+  },
+  appBar: {
+    padding: theme.spacing(1.75, 2)
   }
 }));
 
@@ -20,6 +36,24 @@ export default function AuthLayout({ route }) {
 
   return (
     <div className={classes.wrapper}>
+      <div className={classes.mask} />
+      <AppBar className={classes.appBar}>
+        <GridContainer justifyContent="space-between">
+          <GridItem style={{ display: 'flex', alignItems: 'center' }}>
+            <Typography variant="h5" color="orange">METAFLEX</Typography>
+          </GridItem>
+          <div style={{ display: 'flex' }}>
+            <GridItem>
+              <Button
+                variant="outlined"
+                color="info"
+                startIcon={<HowToReg style={{ fill: 'white' }} />}
+                sx={{ textTransform: 'initial', color: 'white' }}>Register</Button>
+            </GridItem>
+          </div>
+        </GridContainer>
+      </AppBar>
+
       <Suspense fallback={<div>Loading...</div>}>
         {renderRoutes(route.routes)}
       </Suspense>
